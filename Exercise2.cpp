25383f55bf3ec1b4847b1969751cfef5750de917
YaFengYi <468088615@163.com>
2015-05-25 00:09:58 +0800

    类定义
    简单的类定义实例

--- Exercise2.cpp
+++ Exercise2.cpp
@@ -3,6 +3,21 @@
 */
 #include<iostream>
 using namespace std;
+//定义一个坐标类：两个成员变量，两个成员函数
+class Coordinate
+{
+public:
+	int x;
+	int y;
+	void printX()
+	{
+		cout<<"x="<<x<<endl;
+	}
+	void printY()
+	{
+		cout<<"y="<<y<<endl;
+	}
+};
 int main()
 {
 	/*
@@ -48,5 +63,27 @@ int main()
     //释放内存
 	delete []str;
     str=NULL;
+	
+	//定义在栈中的对象
+	Coordinate coordinate;
+	coordinate.x=10;
+	coordinate.y=20;
+	coordinate.printX();
+	coordinate.printY();
+
+	//定义在堆中的对象
+	Coordinate *coordinate1=new Coordinate;
+	if(coordinate1==NULL)
+	{
+		//如果申请内存失败，直接返回
+		return 0;
+	}
+	coordinate1->x=100;
+	coordinate1->y=200;
+	coordinate1->printX();
+	coordinate1->printY();
+	delete coordinate1;
+	coordinate1=NULL;
+	
 	return 0;
 }
